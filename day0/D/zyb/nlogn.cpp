9341bd2a6d08508eed8b899b3e9707011a5ddeba
liuzhangfeiabc <50104096+liuzhangfeiabc@users.noreply.github.com>
2021-05-21 23:30:58 +0800

    Update nlogn.cpp

--- day0/D/zyb/nlogn.cpp
+++ day0/D/zyb/nlogn.cpp
@@ -11,14 +11,21 @@ inline int cx(int x){
 	for(int i = x;i;i -= i & -i) as += t[i];
 	return as;
 }
-#define pb push_back
-int qa[10000010],qb[10000010],qc[10000010],qd[10000010],cnt;
-inline void in(int x,int y){
-	qa[++cnt] = x;qb[cnt] = y;++qc[x];
+int lst,qc[10000010],qd[10000010],tt[10000010];
+inline void in(int x,int k){
+	if(!x){
+		if(k < 0) ans -= (p1[0] + 2 > -k);
+		else ans += (p1[0] + 2 > k + 1);
+	}
+	else if(x == lst){
+		if(k < 0) ans -= lst + 1 - tt[-k];
+		else ans += lst + 1 - tt[k + 1];
+	}
+	else qc[x] = k,++qd[x];
 }
 int main(){
-	int i,j,k,l,r;
-	char ch;n = 0;
+	int i,k,l,r;
+	char ch;n = 0;lst = -1;
 	fread(c,1,10000010,stdin);
 	do{
 		ch = c[n];
@@ -28,14 +35,16 @@ int main(){
 		else break;
 		p1[n] += p1[n - 1];
 		p2[n] = min(p2[n - 1],p1[n]);
+		if(n > 1 && p2[n - 2] >= 0 && p2[n - 1] < 0) lst = n - 2;
 	}while(1);
-	--n;m = n / 2 + 2;
-	for(i = n;i;--i) p3[i] = max(0,p3[i + 1] - p3[i]);
+	if(lst == -1) lst = n - 2;
+	--n;m = 0;
+	for(i = n;i;--i) p3[i] = max(0,p3[i + 1] - p3[i]),m = max(m,p3[i] + 2);
 	st[0] = -1;++ft;
-	int lst = -1;
+	for(i = 0;i <= lst;++i) ++tt[p1[i] + 2];
+	for(i = 1;i <= m;++i) tt[i] += tt[i - 1]; 
 	for(i = 1;i < n;++i){
-		if(i > 1 && p2[i - 2] >= 0 && p2[i - 1] < 0) lst = i - 2;
-		r = lst >= 0 ? lst : i - 1;
+		r = min(lst,i - 1);
 		while(ft && p1[st[ft]] >= p1[i]) --ft; 
 		l = st[ft] + 1;
 		st[++ft] = i;
@@ -44,17 +53,13 @@ int main(){
 			in(r,p3[i + 1]);
 		}
 	}
-	if(lst == -1) lst = n - 2;
-	for(i = 1;i <= lst;++i) qc[i] += qc[i - 1];
-	for(i = 1;i <= cnt;++i) qd[qc[qa[i]]--] = i; 
-	for(i = 0,j = 1;i <= lst;++i){
+	xg(p1[0] + 2);
+	for(i = 1;i < lst;++i){
 		xg(p1[i] + 2);
-		while(j <= cnt && qa[qd[j]] == i){
-			k = qb[qd[j]];
-			if(k < 0) ans -= i + 1 - cx(-k);
-			else ans += i + 1 - cx(k + 1);
-			++j;
-		}
+		if(!qd[i]) continue;
+		k = qc[i];
+		if(k < 0) ans -= 1ll * (i + 1 - cx(-k)) * qd[i];
+		else ans += (i + 1 - cx(k + 1)) * qd[i];
 	}
 	cout<<ans<<'\n';
 	//cerr<<clock()<<endl;
